06cf7a943f3d5727b7594eb2b1bf7bfe0da582c2
Castor Gonzalez de Araujo <a25castorga@teis.intranet>
2025-09-10 17:32:56 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,83 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        // problema 1
+
+        String nombre =  "Fabricio";
+
+        int edad = 30;
+
+        System.out.println("Mi nombre es " + nombre + " y tengo " + edad + " años");
+
+        // problema 2
+
+        final double pi = 3.14;
+
+        int radio = 5;
+
+        System.out.println( pi * (radio * radio));
+
+        // problema 3,4
+
+        int numero = 5;
+
+        if ( numero < 0 ) {
+            System.out.println( " El numero es negativo ");
+
+        } else if ( numero > 0){
+            System.out.println(" El numero es positivo");
+        }
+
+        // problema 5
+
+        int nota = 80;
+
+        if ( nota >= 90 ){
+            System.out.println( " La nota es sobresaliente ");
+        }else if (nota <90 && nota >= 70){
+            System.out.println( " La nota es aprobado ");
+        }else if ( nota < 70){
+            System.out.println( " La no es reprobado");
+        }
+
+        // problema 6
+
+        int numeroA = 5;
+        int numeroB = 10;
+
+        if ( numeroA < numeroB){
+            System.out.println(" a es menor que b");
+        } else {
+            System.out.println(" el numero es mayor");
+        }
+
+        // problema 7
+
+        int n = 5;
+
+        int par = n%2;
+
+        if (par == 0){
+            System.out.println( " el numero es par");
+        } else {
+            System.out.println( " el numero es impar");
+        }
+
+        // problema 8
+
+        int años = 20;
+
+        if ( años < 18 ){
+            System.out.println( " Es menor de edad");
+        } else {
+            System.out.println( " Es mayor de edad ");
+        }
+
+        // problema 9
+
+        char 1 = "lunes";
+        char 2 = "martes";
+        char 3 = "miercoles";
+ }
+}
